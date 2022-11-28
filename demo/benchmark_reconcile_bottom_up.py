7fc41177d3d964483264b8a007433fea0dfb3cb6
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-11-27 22:27:32 -0500

    bottom up reconciliation (full)

---
+++ demo/benchmark_reconcile_bottom_up.py
@@ -0,0 +1,18 @@
+import numpy as np
+import lhts
+from timeit import default_timer as timer
+
+def main():
+    ROOT = "/data/cmu/large-scale-hts-reconciliation/"
+    data_dir = ROOT + "notebooks/"
+
+    S_compact = np.load(open(data_dir + 'm5_hierarchy_parent.npy', 'rb'))
+    y_hat = np.load(open(data_dir + 'm5_prediction_raw/pred_tensor.npy', 'rb'))
+
+    start = timer()
+    print(lhts.reconcile_bottom_up(S_compact, y_hat, 30490, 33549, 4).shape)
+    end = timer()
+    print(end - start)
+
+if __name__ == "__main__":
+    main()
