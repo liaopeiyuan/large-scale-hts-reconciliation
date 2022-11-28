c8b83157b684f3eef9878d5e2e7e735dfe315648
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-11-27 21:39:36 -0500

    Update benchmark_reconcile.py

--- demo/benchmark_reconcile.py
+++ demo/benchmark_reconcile.py
@@ -4,12 +4,12 @@ from timeit import default_timer as timer
 
 
 def main():
-    S = np.random.normal(size=(30000, 30000)).astype(int)
-    G = np.random.normal(size=(30000, 30000)).astype(int)
-    yhat = np.random.normal(size=(30000, 1)).astype(int)
+    S = np.random.normal(0, 1, size=(30000, 30000)).astype(int)
+    G = np.random.normal(size=(30000, 30000))
+    yhat = np.random.normal(size=(30000, 1))
 
     start = timer()
-    print(lhts.det(S, G, yhat).shape)
+    print(lhts.reconcile(S, G, yhat).shape)
     end = timer()
     print(end - start)
 
