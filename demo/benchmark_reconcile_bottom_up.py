dd2562ee74f8cb6b7a384a65fc9a4e02f4e7a413
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-11-27 22:43:10 -0500

    Update benchmark_reconcile_bottom_up.py

--- demo/benchmark_reconcile_bottom_up.py
+++ demo/benchmark_reconcile_bottom_up.py
@@ -7,7 +7,7 @@ def main():
     data_dir = ROOT + "notebooks/"
 
     S_compact = np.load(open(data_dir + 'm5_hierarchy_parent.npy', 'rb'))
-    y_hat = np.load(open(data_dir + 'm5_prediction_raw/pred_tensor.npy', 'rb'))
+    y_hat = np.load(open(data_dir + 'm5_prediction_raw/pred_tensor.npy', 'rb'))[:, 0]
 
     start = timer()
     print(lhts.reconcile_bottom_up(S_compact, y_hat, 30490, 33549, 4).shape)
