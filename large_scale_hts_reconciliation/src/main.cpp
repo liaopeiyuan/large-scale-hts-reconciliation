e3263880b64e1d2f0a6ba5f8594b0ea8fbd0391c
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-11-29 18:50:51 -0500

    Update main.cpp

--- large_scale_hts_reconciliation/src/main.cpp
+++ large_scale_hts_reconciliation/src/main.cpp
@@ -177,8 +177,8 @@ Eigen::MatrixXf construct_reconciliation_matrix(const std::string method,
         throw std::invalid_argument("invalid reconciliation method. Available options are: bottom_up, top_down, middle_out, OLS, WLS");
     }
 
-    //printf("G: %d x %d", G.rows(), G.cols());
-    //printf("S: %d x %d", S.rows(), S.cols());
+    printf("G: %d x %d\n", G.rows(), G.cols());
+    printf("S: %d x %d\n", S.rows(), S.cols());
 
     //G = G(Eigen::seq(slice_start, slice_length), Eigen::all);
     //S = S(Eigen::all, Eigen::seq(slice_start, slice_length));
