8f28a091d41479886c160286ac2189fc8ebcc3f5
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-11-28 19:14:36 -0500

    proper slicing

--- large_scale_hts_reconciliation/src/main.cpp
+++ large_scale_hts_reconciliation/src/main.cpp
@@ -325,11 +325,11 @@ public:
         omp_set_num_threads(24);
         Eigen::MatrixXf y_reconciled = reconcile(method, S_compact, P, yhat_total, level, w, num_base, num_total, num_levels);
     
-        yhat << y_reconciled.topRows(rows[0]);
+        yhat = y_reconciled(Eigen::seqN(0, rows[0]), Eigen::placeholders::all);
 
         int curr_row = rows[0];
         for (int i = 1; i < world_size; i++) {
-            yhats[i] << y_reconciled.middleRows(curr_row, rows[i]);
+            yhats[i] = y_reconciled(Eigen::seqN(curr_row, rows[i]), Eigen::placeholders::all);
             MPI_Isend(yhats[i].data(), rows[i] * cols[i], MPI_FLOAT, 0, 0, comm_global, &reqs[i]);
             curr_row += rows[i];
         }
