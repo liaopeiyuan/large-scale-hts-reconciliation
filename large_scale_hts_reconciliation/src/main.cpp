6a7466c3e6224692396351500702c3a327c18d5e
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-11-29 19:13:33 -0500

    Update main.cpp

--- large_scale_hts_reconciliation/src/main.cpp
+++ large_scale_hts_reconciliation/src/main.cpp
@@ -292,7 +292,7 @@ public:
     MPI_Gather(&ro, 1, MPI_INT, rows.data(), 1, MPI_INT, 0, comm_global);
     MPI_Gather(&co, 1, MPI_INT, cols.data(), 1, MPI_INT, 0, comm_global);
 
-    printf("rank %d: %d %d\n", rows.size(), cols.size());
+    printf("rank %d: %d %d\n", world_rank, rows.size(), cols.size());
 
     int slice_start, slice_length;
     int curr_row = rows[0];
