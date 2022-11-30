d83a58457aaf81a9b172922592ed6a1d9dfe8a7d
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-11-29 19:42:45 -0500

    Update main.cpp

--- large_scale_hts_reconciliation/src/main.cpp
+++ large_scale_hts_reconciliation/src/main.cpp
@@ -179,8 +179,9 @@ Eigen::MatrixXf construct_reconciliation_matrix(const std::string method,
 
     //Eigen::MatrixXf S_slice = S(Eigen::seqN(slice_start, slice_length), Eigen::all).cast<float>();
     //Eigen::MatrixXf G_slice = G(Eigen::all, Eigen::seqN(slice_start, slice_length));
-    G.middleCols(slice_start, slice_length);
-    S.middleRows(slice_start, slice_length).cast<float>();
+    
+    Eigen::MatrixXf G_slice = G.middleCols(slice_start, slice_length);
+    Eigen::MatrixXf S_slice = S.middleRows(slice_start, slice_length).cast<float>();
 
     //printf("G_slice: %d x %d\n", G_slice.rows(), G_slice.cols());
     //printf("S_slice: %d x %d\n", S_slice.rows(), S_slice.cols());
