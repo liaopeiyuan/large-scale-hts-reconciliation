fe7f3ae4895fcbab647ef54d485a0dc7e41dbd1b
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-03 19:19:34 -0500

    Update main.cpp

--- large_scale_hts_reconciliation/src/main.cpp
+++ large_scale_hts_reconciliation/src/main.cpp
@@ -268,6 +268,49 @@ double det(const Eigen::MatrixXd &xs)
   return xs.determinant();
 }
 
+float RMSE(const Eigen::MatrixXf res, const Eigen::MatrixXf gt) {
+    float sum = 0;
+    for (int i = 0; i < res.rows(); i ++) {
+        for (int j = 0; j < res.cols(); j ++) {
+            sum += pow(abs(res(i, j) - gt(i, j)), 2);
+        }
+    }
+    float rmse = sqrt(sum / (res.rows() * res.cols()));
+    return rmse;
+}
+
+float MAE(const Eigen::MatrixXf res, const Eigen::MatrixXf gt) {
+    float sum = 0;
+    for (int i = 0; i < res.rows(); i ++) {
+        for (int j = 0; j < res.cols(); j ++) {
+            sum += abs(res(i, j) - gt(i, j));
+        }
+    }
+    float mae = sum / (res.rows() * res.cols());
+    return mae;
+}
+
+float SMAPE(const Eigen::MatrixXf res, const Eigen::MatrixXf gt) {
+    float sum = 0;
+    for (int i = 0; i < res.rows(); i ++) {
+        for (int j = 0; j < res.cols(); j ++) {
+            float pd = res(i, j);
+            float gtr = gt(i, j);
+            float abse = abs(pd - gtr);
+            float mean = (abs(pd) + abs(gtr)) / 2;
+            if (mean == 0) {
+                sum += 0;
+            } else {
+                float val = abse / mean;
+                sum += val;
+            }
+        }
+    }
+    float smape = sum / (res.rows() * res.cols());
+    return smape;
+}
+
+
 namespace py = pybind11;
 using pymod = pybind11::module;
 
@@ -512,6 +555,9 @@ PYBIND11_MODULE(lhts, m) {
 
     m.def("inv", &inv);
     m.def("det", &det);
+    m.def("RMSE", &RMSE);
+    m.def("MAE", &MAE);
+    m.def("SMAPE", &SMAPE);
 
     m.def("reconcile_matrix", &reconcile_matrix);
     m.def("reconcile", &reconcile);
