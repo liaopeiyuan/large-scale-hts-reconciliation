b441f96dbee671479591b7e071960929fad97807
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-05 00:37:23 -0500

    Create debug_s.py

---
+++ demo/debug_s.py
@@ -0,0 +1,17 @@
+import numpy as np
+import lhts
+from timeit import default_timer as timer
+
+def main():
+    ROOT = "/data/cmu/large-scale-hts-reconciliation/"
+    data_dir = ROOT + "notebooks/"
+
+    S_compact = np.load(open(data_dir + 'm5_hierarchy_parent.npy', 'rb'))
+    S = lhts.construct_S(S_compact)
+    St = np.trnspose(S)
+    G = np.linalg.inv(St * S) * St
+    print(G)
+    
+
+if __name__ == "__main__":
+    main()
