ad5ea948b52cec4fca822da87ba64b716b4810bc
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-05 00:53:11 -0500

    Update main.cpp

--- large_scale_hts_reconciliation/src/main.cpp
+++ large_scale_hts_reconciliation/src/main.cpp
@@ -261,8 +261,10 @@ Eigen::MatrixXf construct_reconciliation_matrix(const std::string method,
         throw std::invalid_argument("invalid reconciliation method. Available options are: bottom_up, top_down, middle_out, OLS, WLS");
     }
 
-    Eigen::MatrixXi S_slice = std::move(S(Eigen::seqN(slice_start, slice_length), Eigen::all));
-    Eigen::MatrixXf G_slice = std::move(G(Eigen::all, Eigen::seqN(slice_start, slice_length)));
+    Eigen::MatrixXi S_slice = S.middleRows(slice_start, slice_length).eval();
+    //std::move(S(Eigen::seqN(slice_start, slice_length), Eigen::all));
+    Eigen::MatrixXf G_slice = G.middleCols(slice_start, slice_length).eval();
+    //std::move(G(Eigen::all, Eigen::seqN(slice_start, slice_length)));
     
     //Eigen::MatrixXf G_slice = Eigen::MatrixXf::Zero(num_base, slice_length);
     //Eigen::MatrixXi S_slice = Eigen::MatrixXi::Zero(slice_length, num_base);
