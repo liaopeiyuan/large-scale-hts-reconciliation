c325a7dd25b33a25ebf3f57e82e44c6bd7b582bc
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-05 00:38:11 -0500

    Update debug_s.py

--- demo/debug_s.py
+++ demo/debug_s.py
@@ -8,7 +8,7 @@ def main():
 
     S_compact = np.load(open(data_dir + 'm5_hierarchy_parent.npy', 'rb'))
     S = lhts.construct_S(S_compact, 5650, 6218, 4)
-    St = np.trnspose(S)
+    St = np.transpose(S)
     G = np.linalg.inv(St * S) * St
     print(G)
     
