8dafd622e0edd0b510684828a67383f48d3270bb
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-07 18:58:41 -0500

    Update benchmark_reconcile_dp.py

--- demo/benchmark_reconcile_dp.py
+++ demo/benchmark_reconcile_dp.py
@@ -43,9 +43,15 @@ def main():
     end = timer()
     elapsed = round(end - start, 4)
     if (rank == size - 1): 
-        print("Top down: ", str(elapsed), " ", lhts.smape(rec, gt))
+        print("Top down (dp matrix): ", str(elapsed), " ", lhts.smape(rec, gt))
         #print(rec[-5:, :])
+
+    start = timer()
     rec2 = distrib.reconcile_dp_optimized("top_down", S_compact, top_down_p, y_hat, -1, 0.0, 5650, 6218, 4)
+    end = timer()
+    elapsed = round(end - start, 4)
+    if (rank == size - 1): 
+        print("Top down (dp optimized): ", str(elapsed), " ", lhts.smape(rec, gt))
 
     start = timer()
     rec = distrib.reconcile_dp_matrix("bottom_up", S_compact, top_down_p, y_hat, -1, 0.0, 5650, 6218, 4)
