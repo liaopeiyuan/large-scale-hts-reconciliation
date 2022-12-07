17380d01b464b7705f97c57dfd6e5af1172c6283
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-07 16:23:19 -0500

    Update main.cpp

--- large_scale_hts_reconciliation/src/main.cpp
+++ large_scale_hts_reconciliation/src/main.cpp
@@ -480,7 +480,7 @@ public:
             int color = (i == world_rank) | (slice_start >= num_base);
             MPI_Comm_split(comm_global, color, world_rank, &comm);
             
-            if (color == 1) {
+            //if (color == 1) {
                 if (i != world_rank) {
                     yhats[i] = Eigen::MatrixXf::Zero(rows[i], cols[i]);
                 } else {
@@ -490,7 +490,7 @@ public:
                 // MPI_Bcast(yhats[i].data(), rows[i] * cols[i], MPI_FLOAT, i, comm_global);
                 MPI_Bcast(yhats[i].data(), rows[i] * cols[i], MPI_FLOAT, i, comm);
                 yhat_total.middleRows(curr_row, rows[i]) = yhats[i].eval();
-            }
+            //}
 
             MPI_Barrier(comm_global);
             curr_row += rows[i];
