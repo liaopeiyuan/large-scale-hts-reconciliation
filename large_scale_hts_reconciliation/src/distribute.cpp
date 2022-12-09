7418950a6ea32370665fa06bea1a0955477f8083
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-09 10:53:27 -0500

    Update distribute.cpp

--- large_scale_hts_reconciliation/src/distribute.cpp
+++ large_scale_hts_reconciliation/src/distribute.cpp
@@ -19,18 +19,10 @@ MatrixXd top_down(const MatrixXi S_compact, const MatrixXd P,
                  "matrix will be used\n";
 
 #pragma omp parallel for
-  for (int i = 0; i < num_nodes; i++) {
+  for (int i = 0; i < num_leaves; i++) {
     int co = S_compact(i, 0);
-    int root = -1;
-    bool is_base = true;
-    for (int j = 1; j < num_levels; j++) {
-      int ro = S_compact(i, j);
-      if (ro == -1) {
-        is_base = false;
-        break;
-      }
-      root = ro;
-    }
+    int root = S_compact(i, num_levels - 1);
+    int is_base = root != -1;
     if (is_base) {
       y.middleRows(co, 1) = P(co, 0) * yhat.middleRows(root, 1);
     }
@@ -56,22 +48,11 @@ MatrixXd middle_out(const MatrixXi S_compact, const MatrixXd P,
                  "matrix will be used\n";
 
 #pragma omp parallel for
-  for (int i = 0; i < num_nodes; i++) {
+  for (int i = 0; i < num_leaves; i++) {
     int co = S_compact(i, 0);
-    int root = co;
     int lvl = num_levels - level;
-    bool is_base = true;
-    for (int j = 1; j < num_levels; j++) {
-      int ro = S_compact(i, j);
-      if (ro == -1) {
-        is_base = false;
-        break;
-      }
-      if (lvl > 0) {
-        root = ro;
-        lvl--;
-      }
-    }
+    int root = S_compact(i, lvl);
+    bool is_base = S_compact(i, num_levels - 1) != -1;
     if (is_base) {
       y.middleRows(co, 1) = P(co, 0) * yhat.middleRows(root, 1);
     }
