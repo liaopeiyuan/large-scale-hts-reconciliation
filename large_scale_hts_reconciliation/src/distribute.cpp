0c23f0c6b56f7abae23dbc247e23fd09070ad54f
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-09 10:16:21 -0500

    Update distribute.cpp

--- large_scale_hts_reconciliation/src/distribute.cpp
+++ large_scale_hts_reconciliation/src/distribute.cpp
@@ -21,17 +21,6 @@ MatrixXd top_down(const MatrixXi S_compact, const MatrixXd P,
     int co = S_compact(i, 0);
     int root = S_compact(i, num_levels - 1);
     int is_base = root != -1;
-    /*
-    int root = -1;
-    bool is_base = true;
-    for (int j = 1; j < num_levels; j++) {
-      int ro = S_compact(i, j);
-      if (ro == -1) {
-        is_base = false;
-        break;
-      }
-      root = ro;
-    }*/
     if (is_base) {
       yhat.middleRows(co, 1) = P(co, 0) * yhat.middleRows(root, 1);
     }
@@ -60,26 +49,9 @@ MatrixXd middle_out(const MatrixXi S_compact, const MatrixXd P,
 #pragma omp parallel for
   for (int i = 0; i < num_leaves; i++) {
     int co = S_compact(i, 0);
-    //int root = co;
     int lvl = num_levels - level;
-    // bool is_base = true;
     int root = S_compact(i, lvl);
     bool is_base = S_compact(i, num_levels - 1) != -1;
-    /*
-    for (int j = 1; j < num_levels; j++) {
-      int ro = S_compact(i, j);
-      if (ro == -1) {
-        is_base = false;
-        break;
-      }
-      if (lvl > 0) {
-        root = ro;
-        lvl--;
-      } else {
-        break;
-      }
-    }
-    */
     if (is_base) {
       yhat.middleRows(co, 1) = P(co, 0) * yhat.middleRows(root, 1);
     }
