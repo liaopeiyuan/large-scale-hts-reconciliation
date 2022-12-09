3686d227ed8bb1f9167f3ef1b88097002577a7aa
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-09 13:27:19 -0500

    Update benchmark_reconcile_mpi.py

--- demo/benchmark_reconcile_mpi.py
+++ demo/benchmark_reconcile_mpi.py
@@ -24,33 +24,35 @@ def main():
     distrib = MPI_utils()
 
     start = timer()
-    #if (rank == size - 1):
-    S_compact = np.load(open(data_dir + 'm5_hierarchy_parent.npy', 'rb'))
-    top_down_p = np.load(open(data_dir + 'm5_prediction_raw/top_down_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
-    level_2_p = np.load(open(data_dir + 'm5_prediction_raw/level_2_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
 
-    #else:
-    #    S_compact, top_down_p = np.array([]), np.array([])
+    S_compact = np.load(open(data_dir + 'm5_hierarchy_parent.npy', 'rb'))
+    
+    if (METHOD == "middle_out"):
+        P = np.load(open(data_dir + 'm5_prediction_raw/top_down_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
+    else:
+        P = np.load(open(data_dir + 'm5_prediction_raw/level_2_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
+
     
     gt = np.load(open(data_dir + 'm5_prediction_raw/mpi/gt_tensor_' + str(rank) + '.npy', 'rb'))
     yhat = np.load(open(data_dir + 'm5_prediction_raw/mpi/pred_tensor_' + str(rank) + '.npy', 'rb'))
 
     start = timer()
-    rec = distrib.reconcile_dp_matrix(METHOD, S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+    rec = distrib.reconcile_dp_matrix(METHOD, S_compact, 5650, 6218, 4, yhat, P, 2, 0.0)
     end = timer()
     elapsed = round(end - start, 4)
     if (rank == size - 1):
+        print(METHOD, ":")
         print("dp matrix: ", str(elapsed), " ", lhts.smape(rec, gt))
 
     start = timer()
-    rec2 = distrib.reconcile_dp_optimized(METHOD, S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+    rec2 = distrib.reconcile_dp_optimized(METHOD, S_compact, 5650, 6218, 4, yhat, P, 2, 0.0)
     end = timer()
     elapsed = round(end - start, 4)
     if (rank == size - 1): 
         print("dp algo: ", str(elapsed), " ", lhts.smape(rec2, gt))
 
     start = timer()
-    rec3 = distrib.reconcile_gather(METHOD, S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+    rec3 = distrib.reconcile_gather(METHOD, S_compact, 5650, 6218, 4, yhat, P, 2, 0.0)
     end = timer()
     elapsed = round(end - start, 4)
     if (rank == size - 1):
