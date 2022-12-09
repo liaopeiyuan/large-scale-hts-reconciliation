b191ef7295a6c7da6a28d6e45a7d170faed1c3a0
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-09 14:57:12 -0500

    Update test_single_process.py

--- large_scale_hts_reconciliation/tests/test_single_process.py
+++ large_scale_hts_reconciliation/tests/test_single_process.py
@@ -2,6 +2,7 @@ import lhts
 import numpy as np
 from timeit import default_timer as timer
 import pytest
+import itertools
 
 ROOT = "/data/cmu/large-scale-hts-reconciliation/"
 data_dir = ROOT + "notebooks/"
@@ -12,6 +13,7 @@ gt = np.load(open(data_dir + 'm5_prediction_raw/gt_tensor.npy', 'rb'))
 top_down_p = np.load(open(data_dir + 'm5_prediction_raw/top_down_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
 level_2_p = np.load(open(data_dir + 'm5_prediction_raw/level_2_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
 
+methods = ["middle_out", "bottom_up", "top_down"]
 modes = ["dense_algo", "sparse_algo", "dense_matrix", "sparse_matrix"]
 
 def run_bottom_up(mode):
@@ -23,6 +25,26 @@ def run_bottom_up(mode):
         return lambda: lhts.reconcile_dense_matrix("bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
     elif mode == "dense_algo":
         return lambda: lhts.reconcile_dense_algo("bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+
+def run_top_down(mode):
+    if mode == "sparse_matrix":
+        return lambda: lhts.reconcile_sparse_matrix("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+    elif mode == "sparse_algo":
+        return lambda: lhts.reconcile_sparse_algo("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+    elif mode == "dense_matrix":
+        return lambda: lhts.reconcile_dense_matrix("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+    elif mode == "dense_algo":
+        return lambda: lhts.reconcile_dense_algo("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+
+def run_middle_out(mode):
+    if mode == "sparse_matrix":
+        return lambda: lhts.reconcile_sparse_matrix("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+    elif mode == "sparse_algo":
+        return lambda: lhts.reconcile_sparse_algo("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+    elif mode == "dense_matrix":
+        return lambda: lhts.reconcile_dense_matrix("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+    elif mode == "dense_algo":
+        return lambda: lhts.reconcile_dense_algo("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
 
 def run_main():
 
@@ -92,9 +114,15 @@ def run_main():
     return True
 
 @pytest.mark.parametrize(
-    "mode", modes
+    "mode,method", itertools.product(modes, methods)
 )
-def test_bottom_up(benchmark, mode):
-    benchmark.group = "bottom_up"
-    result = benchmark(run_bottom_up(mode))
+def test_bottom_up(benchmark, mode, method):
+    benchmark.group = method 
+    if method == "bottom_up":
+        result = benchmark(run_bottom_up(mode))
+    elif method == "middle_out":
+        result = benchmark(run_middle_out(mode))
+    elif method == "top_down":
+        result = benchmark(run_top_down(mode))
+
     assert True
