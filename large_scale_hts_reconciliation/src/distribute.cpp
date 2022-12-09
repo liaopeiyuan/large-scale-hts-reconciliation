4a0880f8287ba32fccd04a1d0aa922bcd192f869
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-09 13:15:41 -0500

    Update distribute.cpp

--- large_scale_hts_reconciliation/src/distribute.cpp
+++ large_scale_hts_reconciliation/src/distribute.cpp
@@ -48,11 +48,22 @@ MatrixXd middle_out(const MatrixXi S_compact, const MatrixXd P,
                  "matrix will be used\n";
 
 #pragma omp parallel for
-  for (int i = 0; i < num_leaves; i++) {
+  for (int i = 0; i < num_nodes; i++) {
     int co = S_compact(i, 0);
+    int root = co;
     int lvl = num_levels - level;
-    int root = S_compact(i, lvl);
-    bool is_base = S_compact(i, num_levels - 1) != -1;
+    bool is_base = true;
+    for (int j = 1; j < num_levels; j++) {
+      int ro = S_compact(i, j);
+      if (ro == -1) {
+        is_base = false;
+        break;
+      }
+      if (lvl > 0) {
+        root = ro;
+        lvl--;
+      }
+    }
     if (is_base) {
       y.middleRows(co, 1) = P(co, 0) * yhat.middleRows(root, 1);
     }
