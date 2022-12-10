06343f1386a2126ffb81a4b704a4e9bc33733f42
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-10 17:58:48 -0500

    update path

--- large_scale_hts_reconciliation/tests/test_mpi_fast.py
+++ large_scale_hts_reconciliation/tests/test_mpi_fast.py
@@ -13,7 +13,7 @@ import numpy as np
 from timeit import default_timer as timer
 
 DATA_ROOT = "m5_hobbies"
-ROOT = "/data/cmu/large-scale-hts-reconciliation/"
+ROOT = "/afs/andrew.cmu.edu/usr23/peiyuanl/private/15-418/large-scale-hts-reconciliation/large_scale_hts_reconciliation" #"/data/cmu/large-scale-hts-reconciliation/"
 data_dir = ROOT + "notebooks/"
 
 S_compact = np.load(open(data_dir + DATA_ROOT + '/m5_hierarchy_parent.npy', 'rb'))    
