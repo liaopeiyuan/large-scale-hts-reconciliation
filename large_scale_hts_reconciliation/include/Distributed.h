a471be9c24c78a907f990cb202d4a0e0c7241e57
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-11 00:10:50 -0500

    try out per-node

--- large_scale_hts_reconciliation/include/Distributed.h
+++ large_scale_hts_reconciliation/include/Distributed.h
@@ -53,6 +53,11 @@ class Distributed {
                             const MatrixXd yhat, const MatrixXd P, int level,
                             double w);
 
+  MatrixXd reconcile_gather_per_node(const std::string method, const MatrixXi S_compact,
+                            int num_leaves, int num_nodes, int num_levels,
+                            const MatrixXd yhat, const MatrixXd P, int level,
+                            double w);
+
   void test(const MatrixXd& xs);
 
  private:
