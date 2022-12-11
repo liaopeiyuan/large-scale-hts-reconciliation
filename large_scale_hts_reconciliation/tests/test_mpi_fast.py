a2978d98bc988cdc9ca765afe7e4a11604c19dc7
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2022-12-11 13:28:22 -0500

    fmt

--- large_scale_hts_reconciliation/tests/test_mpi_fast.py
+++ large_scale_hts_reconciliation/tests/test_mpi_fast.py
@@ -4,8 +4,9 @@ import pytest
 import itertools
 from collections import defaultdict
 import mpi4py
+
 mpi4py.rc.threaded = True
-#mpi4py.rc.thread_level = "funneled"
+# mpi4py.rc.thread_level = "funneled"
 from mpi4py import MPI
 from lhts import Distributed
 import sys
@@ -13,11 +14,13 @@ import numpy as np
 from timeit import default_timer as timer
 
 DATASETS = ["m5_hobbies", "m5_full"]
-num_leaves = [5650, 30490]
-num_nodes = [6218, 33549]
-num_levels = [4, 4]
 
-ROOT = "/home/peiyuan20013/large-scale-hts-reconciliation/large_scale_hts_reconciliation" #"/data/cmu/large-scale-hts-reconciliation/"
+num_leaves = {"m5_hobbies": 5650, "m5_full": 30490}
+num_nodes = {"m5_hobbies": 6218, "m5_full": 33549}
+num_levels = {"m5_hobbies": 4, "m5_full": 4}
+
+
+ROOT = "/home/peiyuan20013/large-scale-hts-reconciliation/large_scale_hts_reconciliation"  # "/data/cmu/large-scale-hts-reconciliation/"
 data_dir = ROOT + "/notebooks/"
 
 S_compacts = {}
@@ -27,15 +30,23 @@ gts = {}
 yhats = {}
 
 for DATA_ROOT in DATASETS:
-    S_compact = np.load(open(data_dir + DATA_ROOT + '/m5_hierarchy_parent.npy', 'rb'))    
-    top_down_p = np.load(open(data_dir + DATA_ROOT +'/top_down_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
-    level_2_p = np.load(open(data_dir + DATA_ROOT + '/level_2_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
+    S_compact = np.load(open(data_dir + DATA_ROOT + "/m5_hierarchy_parent.npy", "rb"))
+    top_down_p = np.load(open(data_dir + DATA_ROOT + "/top_down_tensor.npy", "rb"))[
+        :, 0
+    ].reshape(-1, 1)
+    level_2_p = np.load(open(data_dir + DATA_ROOT + "/level_2_tensor.npy", "rb"))[
+        :, 0
+    ].reshape(-1, 1)
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    gt = np.load(open(data_dir + DATA_ROOT + '/mpi/gt_tensor_' + str(rank) + '.npy', 'rb'))
-    yhat = np.load(open(data_dir + DATA_ROOT + '/mpi/pred_tensor_' + str(rank) + '.npy', 'rb'))
+    gt = np.load(
+        open(data_dir + DATA_ROOT + "/mpi/gt_tensor_" + str(rank) + ".npy", "rb")
+    )
+    yhat = np.load(
+        open(data_dir + DATA_ROOT + "/mpi/pred_tensor_" + str(rank) + ".npy", "rb")
+    )
 
     S_compacts[DATA_ROOT] = S_compact
     top_down_ps[DATA_ROOT] = top_down_p
@@ -46,43 +57,138 @@ for DATA_ROOT in DATASETS:
 methods = ["bottom_up", "top_down", "middle_out"]
 modes = ["dp_matrix", "dp_optimized", "gather"]
 
-def run_bottom_up(mode,dataset):
+
+def run_bottom_up(mode, dataset):
     comm = MPI.COMM_WORLD
     distrib = Distributed()
     if mode == "gather":
-        result = distrib.reconcile_gather("bottom_up", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], top_down_ps[dataset], -1, 1.5)
+        result = distrib.reconcile_gather(
+            "bottom_up",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            top_down_ps[dataset],
+            -1,
+            1.5,
+        )
     elif mode == "dp_matrix":
-        result = distrib.reconcile_dp_matrix("bottom_up", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], top_down_ps[dataset], -1, 1.5)
+        result = distrib.reconcile_dp_matrix(
+            "bottom_up",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            top_down_ps[dataset],
+            -1,
+            1.5,
+        )
     elif mode == "dp_optimized":
-        result = distrib.reconcile_dp_optimized("bottom_up", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], top_down_ps[dataset], -1, 1.5)
+        result = distrib.reconcile_dp_optimized(
+            "bottom_up",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            top_down_ps[dataset],
+            -1,
+            1.5,
+        )
     comm.Barrier()
     return result
 
-def run_top_down(mode,dataset):
+
+def run_top_down(mode, dataset):
     comm = MPI.COMM_WORLD
     distrib = Distributed()
     if mode == "gather":
-        result = distrib.reconcile_gather("top_down", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], top_down_ps[dataset], -1, 1.5)
+        result = distrib.reconcile_gather(
+            "top_down",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            top_down_ps[dataset],
+            -1,
+            1.5,
+        )
     elif mode == "dp_matrix":
-        result = distrib.reconcile_dp_matrix("top_down", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], top_down_ps[dataset], -1, 1.5)
+        result = distrib.reconcile_dp_matrix(
+            "top_down",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            top_down_ps[dataset],
+            -1,
+            1.5,
+        )
     elif mode == "dp_optimized":
-        result = distrib.reconcile_dp_optimized("top_down", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], top_down_ps[dataset], -1, 1.5)
+        result = distrib.reconcile_dp_optimized(
+            "top_down",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            top_down_ps[dataset],
+            -1,
+            1.5,
+        )
     comm.Barrier()
     return result
 
-def run_middle_out(mode,dataset):
+
+def run_middle_out(mode, dataset):
     comm = MPI.COMM_WORLD
     distrib = Distributed()
     if mode == "gather":
-        result = distrib.reconcile_gather("middle_out", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], level_2_p, 2, 1.5)
+        result = distrib.reconcile_gather(
+            "middle_out",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            level_2_p,
+            2,
+            1.5,
+        )
     elif mode == "dp_matrix":
-        result = distrib.reconcile_dp_matrix("middle_out", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], level_2_p, 2, 1.5)
+        result = distrib.reconcile_dp_matrix(
+            "middle_out",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            level_2_p,
+            2,
+            1.5,
+        )
     elif mode == "dp_optimized":
-        result = distrib.reconcile_dp_optimized("middle_out", S_compacts[dataset], num_leaves[dataset], num_nodes[dataset], num_levels[dataset], yhats[dataset], level_2_p, 2, 1.5)
+        result = distrib.reconcile_dp_optimized(
+            "middle_out",
+            S_compacts[dataset],
+            num_leaves[dataset],
+            num_nodes[dataset],
+            num_levels[dataset],
+            yhats[dataset],
+            level_2_p,
+            2,
+            1.5,
+        )
     comm.Barrier()
     return result
- 
+
+
 d = defaultdict(dict)
+
 
 @pytest.mark.mpi()
 def test_ping():
@@ -93,6 +199,7 @@ def test_ping():
     else:
         print("Hello world from rank " + str(rank))
     comm.Barrier()
+
 
 @pytest.mark.mpi()
 @pytest.mark.parametrize(
@@ -105,7 +212,7 @@ def test_ping():
 def test_mpi(benchmark, mode, method, dataset):
 
     benchmark.group = method + "/" + dataset
-    
+
     def f():
         if method == "bottom_up":
             result = run_bottom_up(mode, dataset)
@@ -113,12 +220,12 @@ def test_mpi(benchmark, mode, method, dataset):
             result = run_middle_out(mode, dataset)
         elif method == "top_down":
             result = run_top_down(mode, dataset)
-        return result 
+        return result
 
     result = benchmark.pedantic(f, iterations=1, rounds=10)
-    
+
     d[method][mode] = result
     for (i, j) in itertools.combinations(d[method].values(), 2):
         assert np.allclose(i, j, rtol=1e-3, atol=1e-5)
-    
+
     return
--- large_scale_hts_reconciliation/tests/test_single_process_fast.py
+++ large_scale_hts_reconciliation/tests/test_single_process_fast.py
@@ -5,57 +5,88 @@ import itertools
 from collections import defaultdict
 
 DATA_ROOT = "m5_hobbies"
-ROOT = "/home/peiyuan20013/large-scale-hts-reconciliation/large_scale_hts_reconciliation" #"/data/cmu/large-scale-hts-reconciliation/"
+ROOT = "/home/peiyuan20013/large-scale-hts-reconciliation/large_scale_hts_reconciliation"  # "/data/cmu/large-scale-hts-reconciliation/"
 data_dir = ROOT + "/notebooks/"
 
-S_compact = np.load(open(data_dir + DATA_ROOT + '/m5_hierarchy_parent.npy', 'rb'))
-yhat = np.load(open(data_dir + DATA_ROOT + '/pred_tensor.npy', 'rb'))
-gt = np.load(open(data_dir + DATA_ROOT + '/gt_tensor.npy', 'rb'))
-top_down_p = np.load(open(data_dir + DATA_ROOT + '/top_down_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
-level_2_p = np.load(open(data_dir + DATA_ROOT +'/level_2_tensor.npy', 'rb'))[:, 0].reshape(-1, 1)
+S_compact = np.load(open(data_dir + DATA_ROOT + "/m5_hierarchy_parent.npy", "rb"))
+yhat = np.load(open(data_dir + DATA_ROOT + "/pred_tensor.npy", "rb"))
+gt = np.load(open(data_dir + DATA_ROOT + "/gt_tensor.npy", "rb"))
+top_down_p = np.load(open(data_dir + DATA_ROOT + "/top_down_tensor.npy", "rb"))[
+    :, 0
+].reshape(-1, 1)
+level_2_p = np.load(open(data_dir + DATA_ROOT + "/level_2_tensor.npy", "rb"))[
+    :, 0
+].reshape(-1, 1)
 
 methods = ["middle_out", "bottom_up", "top_down"]
 modes = ["dense_algo", "sparse_algo", "dense_matrix", "sparse_matrix"]
 
+
 def run_bottom_up(mode):
     if mode == "sparse_matrix":
-        return lambda: lhts.reconcile_sparse_matrix("bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_sparse_matrix(
+            "bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
     elif mode == "sparse_algo":
-        return lambda: lhts.reconcile_sparse_algo("bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_sparse_algo(
+            "bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
     elif mode == "dense_matrix":
-        return lambda: lhts.reconcile_dense_matrix("bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_dense_matrix(
+            "bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
     elif mode == "dense_algo":
-        return lambda: lhts.reconcile_dense_algo("bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_dense_algo(
+            "bottom_up", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
+
 
 def run_top_down(mode):
     if mode == "sparse_matrix":
-        return lambda: lhts.reconcile_sparse_matrix("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_sparse_matrix(
+            "top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
     elif mode == "sparse_algo":
-        return lambda: lhts.reconcile_sparse_algo("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_sparse_algo(
+            "top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
     elif mode == "dense_matrix":
-        return lambda: lhts.reconcile_dense_matrix("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_dense_matrix(
+            "top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
     elif mode == "dense_algo":
-        return lambda: lhts.reconcile_dense_algo("top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0)
+        return lambda: lhts.reconcile_dense_algo(
+            "top_down", S_compact, 5650, 6218, 4, yhat, top_down_p, -1, 0.0
+        )
+
 
 def run_middle_out(mode):
     if mode == "sparse_matrix":
-        return lambda: lhts.reconcile_sparse_matrix("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+        return lambda: lhts.reconcile_sparse_matrix(
+            "middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0
+        )
     elif mode == "sparse_algo":
-        return lambda: lhts.reconcile_sparse_algo("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+        return lambda: lhts.reconcile_sparse_algo(
+            "middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0
+        )
     elif mode == "dense_matrix":
-        return lambda: lhts.reconcile_dense_matrix("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+        return lambda: lhts.reconcile_dense_matrix(
+            "middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0
+        )
     elif mode == "dense_algo":
-        return lambda: lhts.reconcile_dense_algo("middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0)
+        return lambda: lhts.reconcile_dense_algo(
+            "middle_out", S_compact, 5650, 6218, 4, yhat, level_2_p, 2, 0.0
+        )
+
 
 d = defaultdict(dict)
 
-@pytest.mark.parametrize(
-    "mode,method", itertools.product(modes, methods)
-)
+
+@pytest.mark.parametrize("mode,method", itertools.product(modes, methods))
 @pytest.mark.mpi_skip()
 def test_single_process_fast(benchmark, mode, method):
-    benchmark.group = method 
-    
+    benchmark.group = method
+
     if method == "bottom_up":
         result = benchmark(run_bottom_up(mode))
     elif method == "middle_out":
